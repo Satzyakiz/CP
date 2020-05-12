e71b948f371235927bd9804566576c08b40b4f28
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2020-05-12 23:10:44 +0530

    CPP template

---
+++ cpp_template.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dbl double
+#define endl "\n"
+#define fo(i,n) for(int i=0; i<n; i++)
+#define FOR(i,n,k) for(int i=0; i<n; i+=k)
+#define sFOR(i,s,n) for(int i=s; i<n; i++)
+#define vi vector<int>
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define eb(x) emplace_back(x)
+#define UM unordered_map<int,int>
+#define INF INT_MAX
+#define NEG_INF INT_MIN
+#define MOD 1000000000+7
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  freopen("D:/CP/input.txt","r",stdin);
+  freopen("D:/CP/output.txt","w",stdout);
+  #endif
+
+  return 0;
+}
+
+// To run g++ new.cpp -std=c++11
