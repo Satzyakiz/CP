ec82e96e82db2ba1aa45b9b065c24af9365b54fe
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2020-07-04 23:14:34 +0530

    additions

--- pblm.cpp
+++ pblm.cpp
@@ -3,21 +3,12 @@ using namespace std;
 #define ll long long
 vector<int> arr;
 void solve(){
-    arr.emplace_back(0);
-    int n,count = 1,x;
-    cin>>n;
-    for(int i=0; i<n; i++){
-        cin>>x;
-        for(int j=0; j<x; j++){
-            arr.emplace_back(i+1);
-        }
-    }
-    int m;
-    cin>>m;
-    for(int i=0; i<m; i++){
-        cin>>x;
-        cout<<arr[x]<<endl;
-    }
+    ll a,b,n,m;
+    cin>>a>>b>>n>>m;
+    if(n <= a && m <= b)
+        cout<<"Yes\n";
+    else
+        cout<<"No\n";
 }
 int main(){
   ios_base::sync_with_stdio(false);
@@ -27,6 +18,9 @@ int main(){
   freopen("D:/CP/input.txt","r",stdin);
   freopen("D:/CP/output.txt","w",stdout);
   #endif
-  solve();
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
   return 0;
 }
