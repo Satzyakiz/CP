b5eeb0e83f3a9a79dcdbac85fb012c04b81ac0fe
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2020-07-23 23:02:59 +0530

    additions

--- Trees/Remove_Half_Nodes.cpp
+++ Trees/Remove_Half_Nodes.cpp
@@ -1,4 +1,4 @@
-// Given A binary Tree. Your task is to remove all the half nodes 
+// Given A binary Tree. Your task is to remove all the half nodes
 // (which has only one child).
 
 Node *RemoveHalfNodes(Node *root)
@@ -14,4 +14,30 @@ Node *RemoveHalfNodes(Node *root)
         return root->left;
     return root;
 
+}
+
+//Another Solution
+void helper(TreeNode *A){
+    if(!A)
+        return;
+    if(!A->left && A->right){
+        A->val = A->right->val;
+        A->left = A->right->left;
+        A->right = A->right->right;
+        helper(A);
+    }
+    else if(A->left!=NULL && A->right==NULL ){
+        A->val = A->left->val;
+        A->right = A->left->right;
+        A->left = A->left->left;
+        helper(A);
+    }
+    else if(A->left!=NULL && A->right!=NULL ){
+        helper(A->left);
+        helper(A->right);
+    }
+}
+TreeNode* Solution::solve(TreeNode* A) {
+    helper(A);
+    return A;
 }
