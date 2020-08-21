4cb42a0f1ec007675400732b8c67fea7882d3b60
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2020-08-21 18:11:59 +0530

    additions

---
+++ Array/Sort_By_Parity.cpp
@@ -0,0 +1,20 @@
+// Given an array A of non-negative integers, return an array
+// consisting of all the even elements of A, followed by all the odd
+// elements of A.
+//
+// You may return any answer array that satisfies this condition.
+// Example 1:
+//
+// Input: [3,1,2,4]
+// Output: [2,4,3,1]
+// The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+vector<int> sortByParity(vector<int> &A){
+    int j = -1;
+    for(int i=0; i<A.size(); i++){
+        if(A[i] % 2 == 0){
+            swap(A[++j],A[i]);
+        }
+    }
+    return A;
+}
