61527572ca96986936a51317d0ec4a2b2c296d9b
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2020-08-27 14:23:05 +0530

    additions

--- pblm.cpp
+++ pblm.cpp
@@ -1,57 +1,41 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int get(string x){
-    reverse(x.begin(),x.end());
-    int ans = 0;
-    for(int i=0; i<x.size(); i++){
-        if(x[i] == '1'){
-            ans += pow(2,i);
-        }
-    }
-    return ans;
-}
-void helper(int m,int ma,int &ans){
-    bitset<32> b1(m), b2(ma);
-    string a = "",b = "";
-    int i;
-    for(i=31; i>=0; i--){
-        if(b1[i] != 0)
-            break;
-    }
-    for( ; i>=0; i--){
-        a += to_string(b1[i]);
-    }
+#define endl "\n"
 
-    for(i=31; i>=0; i--){
-        if(b2[i] != 0)
-            break;
-    }
-    for( ; i>=0; i--){
-        b += to_string(b2[i]);
-    }
-    // cout<<a<<endl<<b<<endl;
-    string x = a + b;
-    string y = b + a;
-    // cout<<x<<endl<<y<<endl;
-    int iX = get(x), iY = get(y);
-    ans = max(ans, iX - iY);
+bool check(vector<vector<int>> mat,int x,int i,int j){
+    if(i < 0 || j < 0 || i >= mat.size() || j >= mat[i].size() || mat[i][j] != x)
+        return false;
+    return true;
+}
+bool checkAll(vector<vector<int>> mat, int i, int j){
+    int x = mat[i][j];
+    return  check(mat,x,i-1,j-1) || check(mat,x,i-1,j) || check(mat,x,i-1,j+1) ||
+            check(mat,x,i,j-1) || check(mat,x,i,j+1) ||
+            check(mat,x,i+1,j-1) || check(mat,x,i+1,j) ||check(mat,x,i+1,j+1);
 }
 void solve(){
-    int n,xx,m = INT_MAX,ma = INT_MIN;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    int ans = INT_MIN;
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            helper(arr[i],arr[j],ans);
+    int m,n;
+    cin>>m>>n;
+    vector<vector<int>> mat(m,vector<int>(n));
+    for(int i=0; i<m; i++)
+        for(int j=0; j<n; j++)
+            cin>>mat[i][j];
+    set<int> s;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+                // cout<<mat[i][j]<<" ";
+                if(mat[i][j] != -1 && !s.count(mat[i][j])){
+                    if(checkAll(mat,i,j)){
+                        s.insert(mat[i][j]);
+                    }
+                }
         }
+        // cout<<endl;
     }
-    // cout<<"==================================\n";
-    cout<<ans<<endl;
+    // cout<<endl;
+    cout<<s.size()<<endl;
+    // cout<<"==============\n";
 }
 int main(){
   ios_base::sync_with_stdio(false);
