2b6881b1d1e8f77481d5ad153c0ec6bc0a558f70
Satzyakiz <40039258+Satzyakiz@users.noreply.github.com>
2021-01-28 22:37:14 +0530

    additions

---
+++ Strings/Smallest_String_with_Given_Numeric_Value.cpp
@@ -0,0 +1,37 @@
+// The numeric value of a lowercase character is defined as its 
+// position (1-indexed) in the alphabet, so the numeric value of a is 1,
+// the numeric value of b is 2, the numeric value of c is 3, and so on.
+//
+// The numeric value of a string consisting of lowercase characters is
+// defined as the sum of its characters' numeric values. For example,
+// the numeric value of the string "abe" is equal to 1 + 2 + 5 = 8.
+//
+// You are given two integers n and k. Return the lexicographically
+// smallest string with length equal to n and numeric value equal to k.
+//
+// Note that a string x is lexicographically smaller than string y
+// if x comes before y in dictionary order, that is, either x is a
+// prefix of y, or if i is the first position such that x[i] != y[i],
+// then x[i] comes before y[i] in alphabetic order.
+//
+// Example 1:
+//
+// Input: n = 3, k = 27
+// Output: "aay"
+//
+// Example 2:
+//
+// Input: n = 5, k = 73
+// Output: "aaszz"
+
+string getSmallestString(int n, int k) {
+    string ans(n, 'a');
+    k -= n;
+    int i = n-1;
+    while(k > 0){
+        ans[i] += min(k, 25);
+        k -= min(k, 25);
+        i--;
+    }
+    return ans;
+}
